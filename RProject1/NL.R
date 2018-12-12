8928a7083bf2d092879df959a452e25bb9a17dc8
Raja Sharath Chandra Guduru <sharath.guduru@Excelra.com>
2018-12-12 20:06:28 +0530

    Add project files.

---
+++ RProject1/NL.R
@@ -0,0 +1,110 @@
+#install.packages("tm")#install.packages('rvest')
+## The easiest way to get dplyr is to install the whole tidyverse:
+#install.packages("tidyverse")
+## Alternatively, install just dplyr:
+#install.packages("dplyr")
+## Or the development version from GitHub:
+#install.packages("devtools")
+#devtools::install_github("tidyverse/dplyr")
+#install.packages("tidyr")
+#install.packages(c('devtools', 'curl'))
+#devtools::install_github('christophergandrud/DataCombine')
+#install.packages("RCurl")
+#install.packages("XML")
+#install.packages("htm2txt")
+#install.packages("SnowballC")
+#Loading the rvest package
+library('rvest')
+library('stringi')
+library('stringr')
+library('tidyverse')
+library('dplyr')
+library('tidyr')
+library('devtools')
+library('RCurl')
+library('XML')
+library('tm')
+library('SnowballC')
+
+#csvFileName <- 'C:\\Users\\sharath.guduru\\source\\repos\\RProject1\\RProject1\\CorpusExcelra.csv'
+
+csvFileName <- "D:\\R\\test1.csv"
+
+df1 <- read.csv(csvFileName, na.strings = "")
+
+#unique(df1$cleanUrlData)
+
+df2 <- data.frame()
+
+#dtm = DocumentTermMatrix(df1,
+                         #control = list(
+                                        #stopwords = TRUE,
+                                        #wordLengths = c(4, 15),
+                                        #removePunctuation = T,
+                                        #removeNumbers = T,
+ ##stemming = T,
+                                        #bounds = list(global = c(minTermFreq, maxTermFreq))
+
+                                        #))
+
+
+
+#write.csv((as.matrix(dtm)), "test.csv")
+
+#dtm.matrix = as.matrix(dtm)
+
+
+for (item in df1) {
+    urlValue <- df1[item, "singleUrl"]
+    newdata <- df1[item, "cleanUrlData"]
+    #print(df1[item, "cleanUrlData"])
+
+    mydata <- Corpus(VectorSource(newdata))
+
+    mydata <- tm_map(mydata, content_transformer(tolower))
+
+    mydata <- tm_map(mydata, content_transformer(gsub), pattern = "\\W", replace = " ")
+
+    removeURL <- function(x) gsub("http[^[:space:]]*", "", x)
+    mydata <- tm_map(mydata, content_transformer(removeURL))
+
+    removeNumPunct <- function(x) gsub("[^[:alpha:][:space:]]*", "", x)
+    mydata <- tm_map(mydata, content_transformer(removeNumPunct))
+
+    mydata <- tm_map(mydata, removeWords, stopwords("english"))
+
+    mydata <- tm_map(mydata, stripWhitespace)
+
+    mydata <- tm_map(mydata, removeNumbers)
+
+    mydata <- tm_map(mydata, removePunctuation)
+
+    mydata <- tm_map(mydata, stemDocument)
+
+    dtm <- TermDocumentMatrix(mydata)
+
+    dtmMatrix <- as.matrix(dtm)    
+        
+    View(dtmMatrix)
+
+    #write.csv(dtmMatrix, "test.csv")
+    #print(dtmMatrix["abhilasha",])
+
+    #print(as.matrix(dtm))
+    #write.csv((as.matrix(dtm)), "test.csv")
+
+    #dtm.matrix = as.matrix(dtm)
+
+    #print(mydata)
+
+    #dtm <- TermDocumentMatrix(mydata)
+    
+    #urlDataFrame = data.frame(urlValue, dtm)
+    
+    #df2 = rbind(urlDataFrame)
+
+    #df2 = rbind(dtm)
+    #print(df2)
+}
+
+#View(dtmMatrix)
---
+++ RProject1/Script.R
@@ -0,0 +1,169 @@
+#install.packages('rvest')
+# The easiest way to get dplyr is to install the whole tidyverse:
+#install.packages("tidyverse")
+# Alternatively, install just dplyr:
+#install.packages("dplyr")
+# Or the development version from GitHub:
+# install.packages("devtools")
+#devtools::install_github("tidyverse/dplyr")
+#install.packages("tidyr")
+#install.packages(c('devtools', 'curl'))
+#devtools::install_github('christophergandrud/DataCombine')
+#install.packages("RCurl")
+#install.packages("XML")
+#install.packages("htm2txt")
+#Loading the rvest package
+library('rvest')
+library('stringi')
+library('stringr')
+library('tidyverse')
+library('dplyr')
+library('tidyr')
+library('devtools')
+library('RCurl')
+library('XML')
+library('tm')
+
+#Specifying the url for desired website to be scraped
+url <- 'https://www.excelra.com/'
+shortUrl <- 'excelra.com'
+csvFileName <- 'excelramainpagetrim.csv' #'cancermainpagetrim.csv'
+csvCorpusFileName <- 'CorpusExcelra.csv'
+
+#Reading the HTML code from the website
+webpage <- read_html(url)
+
+#' Extract link texts and urls from a web page
+#' @param url character an url
+#' @return a data frame of link text and urls
+#' @examples
+#' \dontrun{
+#' scraplinks("http://localhost/")
+#' glinks <- scraplinks("http://google.com/")
+#' }
+#' @export
+scraplinks <- function(url) {
+    # Create an html document from the url
+    webpage <- xml2::read_html(url)
+    # Extract the URLs
+    url_ <- webpage %>%
+        rvest::html_nodes("a") %>%
+        rvest::html_attr("href")
+    # Extract the link text
+    link_ <- webpage %>%
+        rvest::html_nodes("a") %>%
+        rvest::html_text()
+    data_frame = data.frame(link = link_, url = url_)
+    return(data_frame)
+}
+#' Trim whitespace from a string
+#'
+#' `str_trim()` removes whitespace from start and end of string; `str_squish()`
+#' also reduces repeated whitespace inside a string.
+#'
+#' @param string A character vector.
+#' @param side Side on which to remove whitespace (left, right or both).
+#' @return A character vector.
+#' @export
+#' @seealso [str_pad()] to add whitespace
+#' @examples
+#' str_trim("  String with trailing and leading white space\t")
+#' str_trim("\n\nString with trailing and leading white space\n\n")
+#'
+#' str_squish("  String with trailing,  middle, and leading white space\t")
+#' str_squish("\n\nString with excess,  trailing and leading white   space\n\n")
+str_trim <- function(string, side = c("both", "left", "right")) {
+    side <- match.arg(side)
+
+    switch(side,
+    left = stri_trim_left(string),
+    right = stri_trim_right(string),
+    both = stri_trim_both(string)
+  )
+}
+
+#' @export
+#' @rdname str_trim
+str_squish <- function(string) {
+    stri_trim_both(str_replace_all(string, "\\s+", " "))
+}
+
+
+str_remove <- function(string, pattern) {
+    str_replace(string, pattern, "")
+}
+
+#' @export
+#' @rdname str_remove
+str_remove_all <- function(string, pattern) {
+    str_replace_all(string, pattern, "")
+}
+
+clean_link <- function(link) {
+    html = getURL(link, followlocation = TRUE)
+
+    # parse html
+    doc = htmlParse(html, asText = TRUE)
+    plain.text <- xpathSApply(doc, "//text()[not(ancestor::script)][not(ancestor::style)][not(ancestor::noscript)][not(ancestor::form)]", xmlValue)
+    text <- paste(plain.text, collapse = "\n")
+    return(text)
+}
+
+cleanStrings <- function(s) {
+    # first fix the unicode ' characters to be all consistent
+    s <- gsub("\xe2\x80\x99", "'", s, perl = TRUE)
+    s <- gsub("\u0091|\u0092|\u0093|\u0094|\u0060|\u0027|\u2019|\u000A", "'", s, perl = TRUE)
+
+    # Strip unwanted UTF-8 characters
+    s <- iconv(s, "UTF-8", "ASCII", "?")
+    # strip unused characters but leave ' and -
+    # s <- gsub("[^[:alpha:][:space:]'-]", " ", s)
+
+    # now let's get rid of single quotes that are quoted strings and not in the middle of a word
+    # this will leave contractions like don't, I'm, etc.
+    s <- gsub("(?<!\\w)[-'](?<!\\w)", " ", s, perl = TRUE) # just leave - and ' in the middle of words
+
+    s <- gsub("[[:space:]]+", " ", s, perl = TRUE) # consolidate spaces
+    s <- gsub("^[[:space:]]+", "", s, perl = TRUE) # strip leading spaces
+    s <- tolower(s)
+
+    return(s)
+}
+
+
+
+glinks <- scraplinks(url)
+#glinks
+
+cleanupUrls <- unique(str_trim(glinks$url, "both"))
+#write.csv(cleanupUrls, csvFileName)
+
+df2 <- read.csv(csvFileName, na.strings = "")
+df2 <- df2[!(df2$x == "#"),]
+df3 <- df2[!(df2$x == "/"),]
+#df4 <- df3 %>% filter(x != "NA")    -- Very imp function
+df4 <- df3 %>% filter(x != "NA")
+df4$x <- gsub('^/', url, df4$x)
+df4$x <- gsub('^#', paste(url, '#'), df4$x)
+df5 <- dplyr::filter(df4, grepl(shortUrl, x)) #Need to make domain name dynamic
+df6 <- dplyr::filter(df5, !grepl('tel:', x))
+df6 <- dplyr::filter(df5, !grepl('mailto:', x))
+
+
+#allUrls <- df6['x']
+#completeUrlsText <- data.frame()
+#for (urlOfPage in allUrls) {
+    #singleUrl <- str_replace_all(urlOfPage, fixed(" "), "")    
+    #urltextData <- clean_link(singleUrl)
+    #cleanUrlData <- cleanStrings(urltextData)
+    #urlAndText <- data.frame(singleUrl, cleanUrlData)
+    #completeUrlsText <- rbind(urlAndText)    
+#}
+
+#View(completeUrlsText)
+
+df8 <- read.csv(csvCorpusFileName, na.strings = "")
+
+df8
+
+#write.csv(completeUrlsText, "CorpusExcelra.csv")
